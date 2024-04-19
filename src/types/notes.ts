da8731f1edb5fcf4cb67816db91d7ba4028e536f
pravdinalex <pravdinalex@mail.ru>
2024-04-19 23:33:44 +0300

    delete stubs, add storybook

---
+++ src/types/notes.ts
@@ -0,0 +1,26 @@
+enum EOctave {
+  subContr = -3,
+  contr = -2,
+  big = -1,
+  small = 0,
+  first = 1,
+  second = 2,
+  third = 3,
+  forth = 4,
+  fifth = 5,
+}
+
+enum ENote {
+  C = 0,
+  Cs = 0.5,
+  D = 1,
+  Ds = 1.5,
+  E = 2,
+  F = 2.5,
+  Fs = 3,
+  G = 3.5,
+  Gs = 4,
+  A = 4.5,
+  As = 5,
+  B = 5.5,
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    main view
   </main>
 </template>
+
+<script setup lang="ts">
+
+const a: ENote = EOctave['3']
+
+
+</script>
+
